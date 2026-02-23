32c41fda9c04a4ed11d2375814be9e24390f7957
AshDashTooFast3 <338127@student.mboutrecht.nl>
2026-02-23 12:42:52 +0100

    inloggen en uitloggen laten werken

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,10 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'role:Administrator,Medewerker'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
