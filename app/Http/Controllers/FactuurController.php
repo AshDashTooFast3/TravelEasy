d85797b367f9d0d27c1094ad75080c72fa261ce3
Achraf <338127@student.mboutrecht.nl>
2026-03-04 08:52:38 +0100

    routing MVC gemaakt

--- app/Http/Controllers/FactuurController.php
+++ app/Http/Controllers/FactuurController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Factuur;
 
 class FactuurController extends Controller
 {
-    //
+    private $FactuurModel;
+    public function __construct()
+    {
+        $this->FactuurModel = new Factuur;
+    }
+    public function index()
+    {
+        return view('facturatie.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MedewerkerController;
+use App\Http\Controllers\FactuurController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +17,7 @@ Route::get('/dashboard', function () {
 // Alleen Administrators en Managers kunnen deze pagina zien
 Route::middleware(['auth', 'verified', 'role:administrator,manager'])->group(function () {
     Route::get('/management-dashboard', [MedewerkerController::class, 'ManagementDashboard'])->name('management-dashboard');
-    Route::get('/facturatie', [MedewerkerController::class, 'Facturatie'])->name('facturatie.index');
+    Route::get('/facturatie', [FactuurController::class, 'index'])->name('facturatie.index');
 });
 
 
